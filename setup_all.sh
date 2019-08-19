e4636643463d91d1787c1a2b88de071fa3869db0
GeunSam2 <rootiron96@gmail.com>
2019-08-19 21:16:28 +0900

    Update and rename set_all_node.sh to setup_all.sh

---
+++ setup_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IP_LIST="192.168.0.181 192.168.0.182 192.168.0.183 192.168.0.184 192.168.0.185 192.168.0.186 192.168.0.187"
+Password=kbsys1234
+for Node_ip in ${IP_LIST}
+do
+	echo ${Node_ip}
+	sshpass -p ${Password} scp -oStrictHostKeyChecking=no ./install_script.sh root@${Node_ip}:/root/setup_env.sh
+	sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@${Node_ip} chmod 775 /root/setup_env.sh
+	sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@${Node_ip} /root/install_script.sh
+done
+
