6750d3a488246eea5dc30d9be41c5fcd848973cd
GeunSam2 <rootiron96@gmail.com>
2019-10-28 02:38:58 +0900

    Update setup_all.sh

--- setup_all.sh
+++ setup_all.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-MASTER_IP_LIST="187"
-SLAVE_IP_LIST="185 186 246"
-PUBLIC_IP_LIST="182"
+IP_RANGE="192.168.0."
+NODE_IP_LIST="187"
 Password=kbsys1234
 
-for Node_ip in ${MASTER_IP_LIST}
+for Node_ip in ${NODE_IP_LIST}
 do
         echo ${Node_ip}
-        sshpass -p ${Password} scp -oStrictHostKeyChecking=no ./setup-env.sh root@192.168.0.${Node_ip}:/tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} chmod 775 /tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} /tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} bash /tmp/dcos_install.sh master>/tmp/install${Node_ip}.log 2>&1&
-        echo install master started
-done
-
-for Node_ip in ${SLAVE_IP_LIST}
-do
-        echo ${Node_ip}
-        sshpass -p ${Password} scp -oStrictHostKeyChecking=no ./setup-env.sh root@192.168.0.${Node_ip}:/tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} chmod 775 /tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} /tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} bash /tmp/dcos_install.sh slave>/tmp/install${Node_ip}.log 2>&1&
-done
-
-for Node_ip in ${PUBLIC_IP_LIST}
-do
-        echo ${Node_ip}
-        sshpass -p ${Password} scp -oStrictHostKeyChecking=no ./setup-env.sh root@192.168.0.${Node_ip}:/tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} chmod 775 /tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} /tmp/setup_env.sh
-        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@192.168.0.${Node_ip} bash /tmp/dcos_install.sh slave_public>/tmp/install.log 2>&1&
+        sshpass -p ${Password} scp -oStrictHostKeyChecking=no ./setup-env.sh root@${IP_RANG}${Node_ip}:/tmp/setup_env.sh
+        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@${IP_RANG}${Node_ip} chmod 775 /tmp/setup_env.sh
+        sshpass -p ${Password} ssh -oStrictHostKeyChecking=no root@${IP_RANG}${Node_ip} bash /tmp/setup_env.sh > /tmp/setup${Node_ip}.log 2>&1&
+        echo setup-env started at ${Node_ip}
 done
